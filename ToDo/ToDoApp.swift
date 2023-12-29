b64f514ca44c7c98764bf1d3399e6531f2305627
Oleksii Kovtun <kovtun1991@gmail.com>
2023-12-29 02:27:17 +0200

    Create project

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Oleksiy Kovtun on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
